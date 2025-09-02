27501fdf53c824989ee75cac1da0edb840c3d8b7
vishal <vishunyk108@gmail.com>
2025-09-02 16:45:33 +0530

    clean

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:task_manager/Dashboard/dashboard.dart';
 import 'package:task_manager/Login/LoginPage.dart';
-
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Widget _defaultHome = const LoginPage();
+
+  @override
+  void initState() {
+    super.initState();
+    _checkLoginStatus();
+  }
+
+  Future<void> _checkLoginStatus() async {
+    final prefs = await SharedPreferences.getInstance();
+    final isLoggedIn = prefs.getBool("isLoggedIn") ?? false;
+
+    setState(() {
+      _defaultHome = isLoggedIn ? const DashboardPage() : const LoginPage();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +41,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: LoginPage(),
+      home: _defaultHome,
     );
   }
 }
